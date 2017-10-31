aa8679a8f9dd10c4d6a380139ce85110e118dc07
Dereje1 <Dereje1>
2017-10-31 00:19:00 -0400

    heroku

--- src/components/home.js
+++ src/components/home.js
@@ -12,9 +12,9 @@ import Addstock from './addstock'
 import Info from './infomodal'
 
 import io from 'socket.io-client';
-const socket = io('//localhost:3001');
+//const socket = io('//localhost:3001');
 
-//const socket = io('https://dereje-stock-charts.herokuapp.com/');
+const socket = io('https://dereje-stock-charts.herokuapp.com/');
 class Home extends Component {
   constructor(props) {
     super(props);
